cc35bd96f81c15867e2139a86fab6bb2636ae408
Elnur Shahbazov <elem577@gmail.com>
2023-07-18 22:50:48 +0400

    Added smartbee class

--- Sources/SmartBee/SmartBee.swift
+++ Sources/SmartBee/SmartBee.swift
@@ -1,6 +1,16 @@
-public struct SmartBee {
-    public private(set) var text = "Hello, World!"
+import ObjectiveC
 
-    public init() {
+public class SmartBee: NSObject {
+//    public private(set) var text = "Hello, World!"
+    static let config = SmartBee()
+
+    public var token: String?
+    
+    public override init() {
+        
+    }
+    
+    public func eventOccurred(eventType: String, idHash: String, utmSource: String, utmCampaign: String, parameters: Dictionary<String, Any>) {
+        
     }
 }
