51dc784b1cab2c883aafd7e6377291475b33307e
Elnur Shahbazov <elem577@gmail.com>
2023-07-19 23:44:06 +0400

    Demo project add test event

--- SmartBeeDemo/SmartBeeDemo/AppDelegate.swift
+++ SmartBeeDemo/SmartBeeDemo/AppDelegate.swift
@@ -15,9 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        SmartBee.event.token = ""
+        SmartBee.event.token = "test token"
 
-        SmartBee.event.eventOccurred(eventType: "", idHash: "", utmSource: "", utmCampaign: "", parameters: [:])
+        SmartBee.event.eventOccurred(eventType: "test type", idHash: "test id", utmSource: "test source", utmCampaign: "test campaign", parameters: ["test key": "test value"])
         
         return true
     }
